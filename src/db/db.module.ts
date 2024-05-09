f0a074e9b31e195b686c94c0aa0e2686662df00e
Bravoezz <joelmrd1234@gmail.com>
2024-05-09 01:34:01 -0500

    add ssl option

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -13,7 +13,7 @@ export const DatabaseModule: DynamicModule = TypeOrmModule.forRoot({
 	username: process.env.DB_USER_NAME,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
-	// ssl: true,
+	ssl: true,
 	entities: [Product, User], // esto o el autoload en true
 	// autoLoadEntities: true,
 	synchronize: true,
